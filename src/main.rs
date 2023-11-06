3e3616813dcd2acf1e35ec4cb1e8d86a5ae89bfc
Mingya Zhou <mingya.zhou@visual-meaning.com>
2023-11-06 15:57:11 +0000

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use regex::Regex;
 
 fn main() {
-    let edge = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76");
-    let safari = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Version/17.1 Safari/537.36 Edg/118.0.2088.76");
-    is_safari_low_version(&safari);
+    let user_agent = String::from("lorem ipsum");
+    is_safari_low_version(&user_agent);
 }
 
 fn is_safari_low_version(useragent: &String) -> bool {
@@ -22,8 +21,38 @@ fn is_safari_low_version(useragent: &String) -> bool {
             return true;
         } else {
             println!("Not Safari below 17.1");
+            return false;
         }
     } 
     println!("Not a Safari browser");
     return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn safari_16_4() {
+        let safari = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Version/16.4 Safari/537.36 Edg/118.0.2088.76");
+        assert!(is_safari_low_version(&safari)); 
+    }
+
+    #[test]
+    fn safari_17_1() {
+        let safari = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Version/17.1 Safari/537.36 Edg/118.0.2088.76");
+        assert!(!is_safari_low_version(&safari)); 
+    }
+
+    #[test]
+    fn edge() {
+        let edge = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76");
+        assert!(!is_safari_low_version(&edge)); 
+    }
+
+    #[test]
+    fn chrome() {
+        let edge = String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+        assert!(!is_safari_low_version(&edge)); 
+    }
 }
